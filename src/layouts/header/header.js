a259deff7b7667bfbf031a3d11e22075ab7cf4d1
Eddys99 <grama.eddy@yahoo.com>
2022-10-05 21:35:44 +0300

    setup project

---
+++ src/layouts/header/header.js
@@ -0,0 +1,21 @@
+import '../../assets/styles/header.css';
+
+export default function Header() {
+
+    return (
+        <header>
+            <div id = "nav-bar">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/"> Login </a>
+                        </li>
+                        <li>
+                            <a href="/register"> Register </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
